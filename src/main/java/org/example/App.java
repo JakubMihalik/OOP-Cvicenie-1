b76ffa2ad1e2a09d20f36f35398f52bae4bd09ff
JakubMihalik <xmihalikj1@stuba.sk>
2021-02-21 19:28:33 +0100

    Hello World + Scanner

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Ahoj svet");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Zadal si slovo: " + scanner.nextLine());
     }
 }
